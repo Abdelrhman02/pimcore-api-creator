752c2bb91a1cc4ecb034d6d76c4a7a6bd5151ffc
Abdelrhman02 <abdelrhmaningegnere@gmail.com>
2023-10-28 13:37:42 +0200

    adding Datahub bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
     Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle::class => ['all' => true],
 ];
